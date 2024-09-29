e9465054df42effdd6192ee40e5288e00df2fa5a
Vibhanshu <vibhanshudeepakdubey@gmail.com>
2024-09-30 02:58:03 +0530

    updating navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { bagImg, appleImg, searchImg } from '../utils';
+import { navLists } from '../constants';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt='Apple' width={14} height={18} />
+      </nav>
+
+      <div className="flex flex-1 justify-center max-sm:hidden">
+        {navLists.map((nav) => (
+          <div key={nav}>
+            {nav}
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <img src={searchImg} alt="search" width={20} height={18} />
+        <img src={bagImg} alt="bag" width={20} height={18} />
+      </div>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
